00e29d52806168344c2493fc35d08e5a44189f37
Lerato <42895172+Leratoyoungling@users.noreply.github.com>
2018-09-30 01:18:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,20 @@ Submit = function(){
  var name = document.getElementById('name').value;
  var email = document.getElementById('email').value;
  
-  document.write("Survey Submitted!");
-   document.write("Thank You for your participation");
+ if (document.getElementById('poll1').checked) {
+  poll = document.getElementById('poll1').value;
+ } else if (document.getElementById('poll2').checked) {
+  poll = document.getElementById('poll2').value;
+ } else {
+   poll = document.getElementById('poll3').value;
+ }
+  document.write("<h1>Thank You!</h1>");
+  document.write("<p>Your Data is sent to us via email</p>");
+ 
+  document.write("<h3>Here is your data:</h3>");
+ 
+ document.write(name + "<br/>");
+ document.write(email + "<br/>");
+ document.write(poll + "<br/>");
+ 
   }
