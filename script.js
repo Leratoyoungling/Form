4e9613fb3593bf49332ea968b2b9dc479a70f1a5
Lerato <42895172+Leratoyoungling@users.noreply.github.com>
2018-09-29 23:33:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-
+function results() {
+  var name = document.getElementById('name').value;
+  var email = document.getElementById('email').value;
+  
+  document.write("<h1>Thank You!</h1>");
+  document.write("<p>Now, your data will be sent via email."</p>);
+                 
+  document.write("<h3>Here is your data:</h3>");
+  
+  document.write(name + "<br/>");
+  document.write(email + "<br/>");
+}
