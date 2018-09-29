a78867d8c9bdaf5c6812e106ab8b988cbd722ef2
Lerato <42895172+Leratoyoungling@users.noreply.github.com>
2018-09-30 00:51:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 Submit = function(){
-  
-  document.write("<h1>Thank You!</h1>");
-  document.write("<p>Now, your data will be sent via email."</p>);
+ 
+  alert("Survey Submitted!");
+    alert("Thank You for your participation");
   }
