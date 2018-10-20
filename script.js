c11deac44c7e20132157fc026347d88a9c210b78
Lerato <42895172+Leratoyoungling@users.noreply.github.com>
2018-10-20 21:17:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,31 +11,5 @@ feedback_form = function(){
 	
 }
 
-from-submitted = function(){
- 
- var name = document.getElementById('name').value;
- var email = document.getElementById('email').value;
- 
- if (document.getElementById('poll1').checked) {
-  poll = document.getElementById('poll1').value;
- } else if (document.getElementById('poll2').checked) {
-  poll = document.getElementById('poll2').value;
- } else {
-   poll = document.getElementById('poll3').value;
- }
- 
- var comments = document.getElementById('comments').value;
- 
-  document.write("<h1>Thank You!</h1>");
- 
-  document.write("<h3>Here is your data:</h3>");
- 
- document.write(name + "<br/>");
- document.write(email + "<br/>");
- document.write(poll + "<br/>");
- document.write(comments + "<br/>");
- 
-  }
-
 
 
