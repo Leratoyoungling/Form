e0ba1095b2edcd5b5d3076abe70b7ed276ad56ce
Lerato <42895172+Leratoyoungling@users.noreply.github.com>
2018-10-20 23:27:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ feedback_form = function(){
  
  var comments = document.getElementById('comments').value;
  
-  document.write("Thank You!".fontcolor('red'));
+  document.write("<h1>Thank You!</h1>".fontcolor('red'));
  
   document.write("<h3>Here is your data:</h3>");
  
